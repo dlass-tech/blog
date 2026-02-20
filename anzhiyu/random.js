6442c2314ccee1c5b5c305a487f1badaa9edbe4f
Super-Yyt <super_ye@outlook.com>
2026-02-20 08:32:05 +0000

    deploy: d2331e1571507c58f7040f89ba3643464cbd620d

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/20/hello_world/","2026/02/20/xy/index/"];function toRandomPost(){
+var posts=["2026/02/20/hello_world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
