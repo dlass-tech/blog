2ee3c34d5dbaa7bbb223ddf1634392d8ec1526a6
Super-Yyt <super_ye@outlook.com>
2026-02-26 06:11:06 +0000

    deploy: f09820a69e5025a443b3a2cdd59a90c8b82b5699

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/21/Welcome!/","2026/02/21/classworks/","2026/02/20/hello_world/","2026/02/25/henan-eg/","2026/02/20/iccce/","2026/02/20/inkeys/","2026/02/20/dlass/"];function toRandomPost(){
+var posts=["2026/02/21/Welcome!/","2026/02/21/classworks/","2026/02/20/dlass/","2026/02/20/hello_world/","2026/02/25/henan-eg/","2026/02/20/iccce/","2026/02/20/inkeys/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
